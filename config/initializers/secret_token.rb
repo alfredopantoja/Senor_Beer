0e391c5266ee57a4329b346b0868e4a2cf77e8ef
Alfredo Pantoja <chololoy@gmail.com>
2013-06-23 06:24:10 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SenorBeer::Application.config.secret_key_base = '1e22a08f342af7b54139283e5d7dd8090ae4a6c8433e9c788c5fea3ce560eee5e774ecc1c30566c2c2b9e0d7cc0abf42a00a972b77571e234ef9c6332d72f834'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SenorBeer::Application.config.session_store :cookie_store, key: '_senor_beer_session'
