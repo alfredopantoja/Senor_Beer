7cbc72018b9e2d4a295993c9b88e438654fd91de
Alfredo Pantoja <chololoy@gmail.com>
2013-07-02 16:25:32 -0500

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Alfredo Pantoja"
+    email    "pantoja@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end  
